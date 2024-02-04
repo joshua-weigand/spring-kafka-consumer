f524e2b9bffe72df5e0acc3cd630978f94b0b826
Joshua Weigand <joshua.weigand@contrivance.app>
2024-02-04 11:44:15 -0700

    chore: renamed main class

--- src/main/java/com/github/weigand/Application.java
+++ src/main/java/com/github/weigand/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringKafkaConsumerApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringKafkaConsumerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
